f26edb8b34a63db57f54be7ba0e2eea537d7cfbd
sitthmueeain <yoopajdoto@gmail.com>
2023-01-17 00:34:26 -0800

    design_unresponsive

---
+++ index.js
@@ -0,0 +1,12 @@
+function scrollElement(){
+    const element = document.getElementById("sec")
+    element.scrollIntoView(element)
+}
+function scrollElement(){
+    const service = document.getElementById("service")
+    service.scrollIntoView(service)
+}
+function scrollElement(){
+    const feedback = document.getElementById("feedback")
+    feedback.scrollIntoView(feedback)
+}
